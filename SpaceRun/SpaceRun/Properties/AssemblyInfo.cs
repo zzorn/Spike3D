2827f805cc06adf12fe0bafc2f3c633d7bf39fce
Dzarg <none>
2010-05-20 10:41:37 +0300

    Rename part 2.

---
+++ SpaceRun/SpaceRun/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Spacerun")]
+[assembly: AssemblyProduct("Spacerun")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Avek Esitysratkaisut Oy")]
+
+[assembly: AssemblyCopyright("Copyright © Avek Esitysratkaisut Oy 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a10fc519-55af-44f4-bbda-c7d6b069fae5")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
