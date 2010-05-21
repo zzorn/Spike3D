179b60c067ebe53229654d8fb305189fa15779fc
unknown <Lari Unkari@.(none)>
2010-05-21 13:33:45 +0300

    Moved helper function QuaternionToEuler() to a new Utilities class.

--- SpaceRun/SpaceRun/PlayerShip.cs
+++ SpaceRun/SpaceRun/PlayerShip.cs
@@ -53,7 +53,7 @@ namespace SpaceRun
 
         public void DrawDebug(SpriteBatch spriteBatch, SpriteFont font)
         {
-            Vector3 playerShipEulerAngles = QuaternionToEuler(heading);
+            Vector3 playerShipEulerAngles = Utilities.QuaternionToEuler(heading);
 
             spriteBatch.DrawString(
                     font,
@@ -64,51 +64,6 @@ namespace SpaceRun
                     "PlayerShip hull[" + Math.Round(hull) + "]\n",
                     new Vector2(10.0f, 10.0f),
                     Color.Yellow);
-        }
-
-        /// <summary> 
-        /// The function converts a Microsoft.Xna.Framework.Quaternion into a Microsoft.Xna.Framework.Vector3 
-        /// </summary> 
-        /// <param name="q">The Quaternion to convert</param> 
-        /// <returns>An equivalent Vector3</returns> 
-        /// <remarks> 
-        /// This function was extrapolated by reading the work of Martin John Baker. All credit for this function goes to Martin John. 
-        /// http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToEuler/index.htm 
-        /// </remarks> 
-        private Vector3 QuaternionToEuler(Quaternion q) 
-        { 
-            Vector3 v = new Vector3(); 
- 
-            v.X = (float)Math.Atan2 
-            ( 
-                2 * q.Y * q.W - 2 * q.X * q.Z,  
-                1 - 2*Math.Pow(q.Y, 2) - 2*Math.Pow(q.Z, 2) 
-            ); 
- 
-            v.Y = (float)Math.Asin 
-            ( 
-                2*q.X*q.Y + 2*q.Z*q.W 
-            ); 
- 
-            v.Z = (float)Math.Atan2 
-            ( 
-                2*q.X*q.W-2*q.Y*q.Z, 
-                1 - 2*Math.Pow(q.X, 2) - 2*Math.Pow(q.Z, 2) 
-        ); 
- 
-            if(q.X*q.Y + q.Z*q.W == 0.5) 
-            { 
-                v.X = (float)(2 * Math.Atan2(q.X,q.W)); 
-                v.Z = 0;     
-            } 
- 
-            else if(q.X*q.Y + q.Z*q.W == -0.5) 
-            { 
-                v.X = (float)(-2 * Math.Atan2(q.X, q.W)); 
-                v.Z = 0; 
-            } 
- 
-            return v; 
         }
     }
 }
