05d13088316a6d5fa674a68441f32f3b50af2e18
Dzarg <Dzarg>
2010-05-21 16:03:39 +0300

    Added roll.

--- SpaceRun/SpaceRun/PlayerShip.cs
+++ SpaceRun/SpaceRun/PlayerShip.cs
@@ -25,8 +25,9 @@ namespace SpaceRun
             base.LogicUpdate(time, t);
 
             thrustVector_N = -new Vector3(0, 0, GamePad.GetState(PlayerIndex.One).Triggers.Right) * 100000.0f;
-            torqueThrustVector_N = new Vector3(GamePad.GetState(PlayerIndex.One).ThumbSticks.Left, 0) * 10000.0f;
-            torqueThrustVector_N = new Vector3(-torqueThrustVector_N.X, torqueThrustVector_N.Y, 0) ;
+            torqueThrustVector_N = new Vector3(GamePad.GetState(PlayerIndex.One).ThumbSticks.Left, GamePad.GetState(PlayerIndex.One).ThumbSticks.Right.X) * 10000.0f;
+            torqueThrustVector_N = new Vector3(-torqueThrustVector_N.X, torqueThrustVector_N.Y, -torqueThrustVector_N.Z / 10);
+            
 
             if (torqueThrustVector_N.LengthSquared() == 0)
             {
